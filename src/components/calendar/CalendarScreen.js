3f13bd6a2d6f52fadfd2e3d96cf7fd428c6d8389
Martin Obermeier <martin.obermeier@gmail.com>
2021-07-14 22:06:01 -0300

    Seccion 23: MERN Calendar - Estructura y Disenio

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment'
+
+import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// Cambio el idioma
+import 'moment/locale/es';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiOpenModal } from '../../actions/ui';
+import { eventClearActive, eventSetActive } from '../../actions/events';
+import { AddNewFab } from '../ui/AddNewFab';
+import { DeleteEventFab } from '../ui/DeleteEventFab';
+moment.locale('es');
+
+const localizer = momentLocalizer( moment );
+
+// const events = [{
+//     title:'Cumpleaños del jefe',
+//     start: moment().toDate(), //new Date()
+//     end: moment().add( 2, 'hours' ).toDate(),
+//     bgcolor: '#fafafa',
+//     notes: 'Comprar el pastel',
+//     user: {
+//         _id: '123',
+//         name: 'Martin',
+//     }
+// }]
+
+export const CalendarScreen = () => {
+
+    const dispatch = useDispatch();
+    
+    const { events, activeEvent } = useSelector( state => state.calendar );
+
+    const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' );
+
+    const onDoubleClick = (e) => {
+        //console.log(e);
+        dispatch( uiOpenModal() );
+    }
+
+    const onSelectEvent = (e) => {
+        //console.log(e);
+        dispatch( eventSetActive(e) );
+    }
+
+    const onViewChange = (e) => {
+        //console.log(e);
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
+
+    const onSelectSlot = (e) => {
+        //console.log(e);
+        dispatch( eventClearActive() );
+    }
+
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        }
+
+        return {
+            style
+        }
+    }
+
+    return (
+        <div className="calendar-screen">
+            <Navbar />
+
+            <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                onSelectSlot={ onSelectSlot }
+                selectable={ true }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+
+            <AddNewFab />
+
+            { ( activeEvent ) && <DeleteEventFab /> }
+    
+            <CalendarModal />
+
+        </div>
+    )
+}
---
+++ src/types/types.js
@@ -0,0 +1,12 @@
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventSetActive: '[event] Set active',
+    eventAddNew: '[event] Add new',
+    eventClearActive: '[event] Clear active',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    
+}
